3878cbdc9c802ab8de43012c0aa7e211c45ff782
danilojezernik <dani.jezernik@gmail.com>
2023-07-14 17:26:02 +0200

    angular todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TaskListComponent } from './task-list/task-list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
+import {TaskListModule} from "./task-list/task-list.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TaskListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
